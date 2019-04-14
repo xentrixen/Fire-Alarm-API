b2b6980abfc031b0ab480e4784a7cf09f3cb58fb
Inori Yuzuriha <drawangel1234@yahoo.com>
2019-04-14 20:27:13 +0800

    Test

--- app/Http/Resources/FireReport.php
+++ app/Http/Resources/FireReport.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Citizen extends JsonResource
+class FireReport extends JsonResource
 {
     /**
      * Transform the resource into an array.
