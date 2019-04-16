1a8add49f43048a120a1d2c6214b43820334787d
Inori Yuzuriha <drawangel1234@yahoo.com>
2019-04-17 00:49:04 +0800

    Test

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -14,7 +14,7 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Broadcast::routes(['middleware' => ['api', 'multiauth:admin,citizen,fire-personnel']]);
+        Broadcast::routes(['middleware' => ['auth:api']]);
 
         require base_path('routes/channels.php');
     }
