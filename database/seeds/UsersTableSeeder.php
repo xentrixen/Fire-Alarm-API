23ebba98f6927548ab82a4cf1484741828fbf070
Inori Yuzuriha <drawangel1234@yahoo.com>
2019-04-16 23:34:12 +0800

    Test

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 
 use App\Admin;
 use App\Citizen;
+use App\FireStation;
 use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
@@ -28,6 +29,15 @@ class UsersTableSeeder extends Seeder
             'password' => Hash::make('password'),
             'active' => true,
             'activation_token' => str_random(60)
+        ]);
+
+        FireStation::truncate();
+        FireStation::create([
+            'name' => 'Example Station',
+            'username' => '1231234',
+            'latitude' => 10.246575,
+            'longitude' => 123.814931,
+            'password' => Hash::make('password')
         ]);
     }
 }
