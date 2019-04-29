7fa63805a71a04a18c16d291a1f24dc493b60a90
Inori Yuzuriha <drawangel1234@yahoo.com>
2019-04-29 21:56:13 +0800

    Test

---
+++ database/migrations/2019_04_29_215400_drop_unique_constraint_citizens_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropUniqueConstraintCitizensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('citizens', function(Blueprint $table) {
+            $table->dropUnique('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
