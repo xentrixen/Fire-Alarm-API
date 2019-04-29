bd123f604304f08f3fa4b77127dfd3aea9c02e5d
Inori Yuzuriha <drawangel1234@yahoo.com>
2019-04-29 21:58:03 +0800

    Test

--- database/migrations/2019_04_29_215400_drop_unique_constraint_citizens_table.php
+++ database/migrations/2019_04_29_215400_drop_unique_constraint_citizens_table.php
@@ -14,7 +14,7 @@ class DropUniqueConstraintCitizensTable extends Migration
     public function up()
     {
         Schema::table('citizens', function(Blueprint $table) {
-            $table->dropUnique('email');
+            $table->dropUnique('citizens_email_unique');
         });
     }
 
